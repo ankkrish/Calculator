bb25f703512817f57ac92c5e13f6ea3b46fdfadb
Ankit Krishna <ankitkrishna8@gmail.com>
2024-09-11 21:15:42 +0530

    Calculator App using Java and Swing.

---
+++ Calculator/src/main/java/com/ankkrish/calculator/App.java
@@ -0,0 +1,10 @@
+package com.ankkrish.calculator;
+
+import com.ankkrish.calculator.ui.CalculatorUI;
+
+public class App {
+
+    public static void main(String[] args) {
+        new CalculatorUI();
+    }
+}
